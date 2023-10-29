138dea93d8fd52ecf7d0d7995b15bb1147fcc88e
ignasvilkas <ignas.pilybas@icloud.com>
2023-10-29 10:47:27 +0000

    added hide class to buttons in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,17 +91,12 @@ const questions = [
   } 
 ]
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 603fb6d72366d859516f3c0aa6ca25ea12816191
 const questionElement = document.getElementById('question');
 const answerButtons = document.querySelectorAll('.answer-btn');
 const startButton = document.getElementById('startButton');
 const nextButton = document.getElementById('nextButton');
 let currentQuestionIndex = 0;
-<<<<<<< HEAD
-let score = 0; // To track the score
+let score = 0;
 
 answerButtons.forEach((button, index) => {
   button.addEventListener('click', () => handleAnswerClick(index));
@@ -115,21 +110,7 @@ function handleAnswerClick(selectedIndex) {
 }
 
 function startQuiz() {
-  score = 0; // Reset the score when the quiz starts
-=======
-
-startButton.addEventListener('click', startQuiz);
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++;
-  if (currentQuestionIndex < questions.length) {
-    showQuestion(questions[currentQuestionIndex]);
-  } else {
-    showResults();
-  }
-});
-
-function startQuiz() {
->>>>>>> 603fb6d72366d859516f3c0aa6ca25ea12816191
+  score = 0;
   startButton.classList.add('hide');
   currentQuestionIndex = 0;
   showQuestion(questions[currentQuestionIndex]);
@@ -138,63 +119,34 @@ function startQuiz() {
 function showQuestion(question) {
   questionElement.innerText = question.question;
   nextButton.classList.add('hide');
-<<<<<<< HEAD
   
   answerButtons.forEach((button, index) => {
     button.innerText = question.answers[index].text;
     button.classList.remove('correct', 'wrong', 'selected');
   });
 
-=======
-  answerButtons.forEach((button, index) => {
-    button.innerText = question.answers[index].text;
-    button.classList.remove('correct', 'wrong', 'selected');
-    button.addEventListener('click', () => checkAnswer(index, question));
-  });
-  // Show answer buttons
->>>>>>> 603fb6d72366d859516f3c0aa6ca25ea12816191
   answerButtons.forEach(button => {
     button.style.display = 'block';
   });
 }
 
-<<<<<<< HEAD
 function checkAnswer(selectedIndex, question) {
   if (question.answers[selectedIndex].correct) {
-    score++; // Increase score if selected answer is correct
+    score++;
   }
 
   answerButtons.forEach((button, index) => {
     if (question.answers[index].correct) {
-=======
-
-function checkAnswer(selectedIndex, question) {
-  const currentQuestion = question;
-  answerButtons.forEach((button, index) => {
-    if (currentQuestion.answers[index].correct) {
->>>>>>> 603fb6d72366d859516f3c0aa6ca25ea12816191
       button.classList.add('correct');
     } else {
       button.classList.add('wrong');
     }
   });
-<<<<<<< HEAD
   
-=======
-  // Change the correct answer to green
-  answerButtons.forEach((button, index) => {
-    if (currentQuestion.answers[index].correct) {
-      button.classList.remove('wrong');
-      button.classList.add('correct');
-    }
-  });
-  // Mark the selected answer
->>>>>>> 603fb6d72366d859516f3c0aa6ca25ea12816191
   answerButtons[selectedIndex].classList.add('selected');
   nextButton.classList.remove('hide');
 }
 
-<<<<<<< HEAD
 function goToNextQuestion() {
   currentQuestionIndex++;
   if (currentQuestionIndex < questions.length) {
@@ -218,23 +170,11 @@ function homePage() {
   questionElement.innerText = "General Knowledge Quiz";
   startButton.classList.remove('hide');
   nextButton.classList.add('hide');
-  nextButton.innerText = 'Next Question'; // Reset the nextButton text
+  nextButton.innerText = 'Next Question';
   nextButton.removeEventListener('click', homePage);
   nextButton.addEventListener('click', goToNextQuestion);
-  score = 0; // Reset score
-  currentQuestionIndex = 0; // Reset question index
+  score = 0;
+  currentQuestionIndex = 0;
 }
 
 
-=======
-
-function showResults() {
-  questionElement.innerText = 'Quiz Completed';
-  answerButtons.forEach((button) => {
-    button.classList.add('hide');
-  });
-  nextButton.innerText = 'Restart Quiz';
-  nextButton.classList.remove('hide');
-  nextButton.addEventListener('click', startQuiz);
-}
->>>>>>> 603fb6d72366d859516f3c0aa6ca25ea12816191
